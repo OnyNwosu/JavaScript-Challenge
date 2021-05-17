0bf04a886069ca97246b09bd9698e3d51f3e7ddc
OnyNwosu <onuwa.nwosu@gmail.com>
2021-05-17 18:24:29 -0500

    first part

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,33 @@
 // from data.js
 var tableData = data;
+var tableBody = d3.select('tbody')
 
-// YOUR CODE HERE!
+function generateTable(data) {
+    tableBody.html('')
+    // console.log(data) -> used for testing 
+    data.forEach((row) => {
+        // Append a row to the table body
+        const tableRow = tableBody.append("tr");
+        // Loop through each field in the row and add each value as a table cell (td)
+        Object.values(row).forEach((val) => {
+            let cell = tableRow.append("td");
+            cell.text(val);
+        }
+        );
+    })
+}
+
+function filterButtonClicked() {
+    var filteredData = tableData;
+    // console.log('Button Clicked')-> used for testing
+    var dateText = d3.select('#datetime').property('value');
+    
+    if (dateText){
+        filteredData = filteredData.filter(row => row.datetime === dateText )
+    }
+    generateTable(filteredData)
+}
+
+d3.select('#filter-btn').on('click', filterButtonClicked)
+
+generateTable(tableData)
